58dc862ad7c8f9ebf2beeeaea94980321f1d6f36
Shobola <shorustamovismoilbek2@gmail.com>
2025-10-27 10:38:14 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
-
-import React from 'react'
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome to UniHero</h1>
       <p>Your education companion powered by UniHero!</p>
+      <div>
+        <Link href="https://t.me/UniHero_news">
+          <a>Join the Community</a>
+        </Link>
+        <Link href="/about">
+          <a>Learn More</a>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
