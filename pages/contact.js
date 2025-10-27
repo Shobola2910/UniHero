077d8fe25e0b87f3f450b211c3a19d0022873209
Shobola <shorustamovismoilbek2@gmail.com>
2025-10-27 11:01:02 -0400

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -2,8 +2,11 @@
 import { useState } from 'react';
 import CommentForm from '../components/CommentForm'; // Import your comment form component
 
+// pages/contact.js
 export default function Contact() {
-  return (
+  return <div>Contact Page</div>;
+}
+
     <div>
       <h1>Contact Us</h1>
       <p>Leave a comment and connect with us!</p>
