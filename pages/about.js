207b1622a0745a13052901373575f4d50c4af332
Shobola <shorustamovismoilbek2@gmail.com>
2025-10-27 10:38:29 -0400

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,1 +1,17 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About UniHero</h1>
+      <div>
+        <p>Our journey from 2024 to 2025...</p>
+        <img src="/images/1-unihero-bot-created.png" alt="UniHero Bot Created" />
+        <div>
+          <p>🤖 2024 · Dec: UniHero Bot created</p>
+          <p>🧑🤝🧑 2024 · Oct: 2 anonymous founders</p>
+          {/* Add other timeline items similarly */}
+        </div>
+      </div>
+    </div>
+  );
+}
 
