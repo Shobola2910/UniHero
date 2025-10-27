c5e4e06aa2078147de93b7ce377b4b6eeeb5b0ad
Shobola <shorustamovismoilbek2@gmail.com>
2025-10-27 10:44:32 -0400

    Update resources.js

--- pages/resources.js
+++ pages/resources.js
@@ -1,1 +1,11 @@
+// pages/resources.js
+export default function Resources() {
+  return (
+    <div>
+      <h1>Resources</h1>
+      <p>Check out the following resources to help you succeed!</p>
+      {/* Add resource links or sections here */}
+    </div>
+  );
+}
 
