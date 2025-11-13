017f7978d9c0f991fcb5b6ccf9bef140092713ac
Shobola <shorustamovismoilbek2@gmail.com>
2025-11-13 18:29:01 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        darkNavy: '#001B48',
+        navyBlue: '#02457A',
+        brightBlue: '#018ABE',
+        lightBlue: '#97CADB',
+        veryLightBlue: '#D6EBEE',
+      },
+    },
+  },
+  plugins: [],
+};
