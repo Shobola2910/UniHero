ae10f5901908296d886abbf0253e8b8dcf4fe553
Shobola <shorustamovismoilbek2@gmail.com>
2025-11-13 18:28:17 -0500

    Update motivation.tsx

--- pages/motivation.tsx
+++ pages/motivation.tsx
@@ -1,1 +1,14 @@
+// pages/motivation.tsx
 
+import MotivationQuotes from '../components/MotivationQuotes';
+
+const MotivationPage = () => {
+  return (
+    <div className="motivation-page">
+      <h2>Motivational Quotes</h2>
+      <MotivationQuotes />
+    </div>
+  );
+};
+
+export default MotivationPage;
