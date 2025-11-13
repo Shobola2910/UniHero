d8bbe0eeacbe570489a66c7f0105d7b874ef08c8
Shobola <shorustamovismoilbek2@gmail.com>
2025-11-13 18:41:10 -0500

    Update resources.tsx

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,13 +1,23 @@
 // pages/resources.tsx
 
-import ResourceLink from '../components/ResourceLink';
+import { useState } from 'react';
+import AssignmentsModal from '../components/AssignmentsModal';
+import ExamPrepModal from '../components/ExamPrepModal';
 
 const ResourcesPage = () => {
+  const [isAssignmentsModalOpen, setAssignmentsModalOpen] = useState(false);
+  const [isExamPrepModalOpen, setExamPrepModalOpen] = useState(false);
+
   return (
     <div className="resources-page">
       <h2>Resources & Assignments</h2>
-      <ResourceLink label="Assignments" botLink="https://t.me/UniHero_BOT?start=assignments" />
-      <ResourceLink label="Exam Prep" botLink="https://t.me/UniHero_BOT?start=examprep" />
+      <div className="resource-buttons">
+        <button onClick={() => setAssignmentsModalOpen(true)}>Assignments</button>
+        <button onClick={() => setExamPrepModalOpen(true)}>Exam Prep</button>
+      </div>
+
+      <AssignmentsModal isOpen={isAssignmentsModalOpen} onClose={() => setAssignmentsModalOpen(false)} />
+      <ExamPrepModal isOpen={isExamPrepModalOpen} onClose={() => setExamPrepModalOpen(false)} />
     </div>
   );
 };
