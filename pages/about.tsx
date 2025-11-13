6476a4319802a60a6c99be15c9071bbfd25f0eb8
Shobola <shorustamovismoilbek2@gmail.com>
2025-11-13 18:19:11 -0500

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,1 +1,15 @@
+// pages/about.tsx
+
+import TimelineSlider from '../components/TimelineSlider';
+
+const AboutPage = () => {
+  return (
+    <div className="about-page">
+      <h2>About Us</h2>
+      <TimelineSlider />
+    </div>
+  );
+};
+
+export default AboutPage;
 
