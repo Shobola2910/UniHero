e184e879a733392c70eb4a76d348c5dbdc5c61c4
Shobola <shorustamovismoilbek2@gmail.com>
2025-11-13 18:23:14 -0500

    Create StudyGuides.tsx

---
+++ components/StudyGuides.tsx
@@ -0,0 +1,16 @@
+// components/StudyGuides.tsx
+
+const StudyGuides = () => {
+  return (
+    <div className="study-guides">
+      <h2>Study Guides</h2>
+      <ul>
+        <li><a href="https://link-to-study-guide-1.com">University Study Guide 1</a></li>
+        <li><a href="https://link-to-study-guide-2.com">University Study Guide 2</a></li>
+        <li><a href="https://link-to-study-guide-3.com">University Study Guide 3</a></li>
+      </ul>
+    </div>
+  );
+};
+
+export default StudyGuides;
