25c3a0f0e11ff86f2e759c13ae8391b0128d47d1
Shobola <shorustamovismoilbek2@gmail.com>
2025-11-13 18:28:42 -0500

    Update resources.tsx

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,1 +1,15 @@
+// pages/resources.tsx
 
+import ResourceLink from '../components/ResourceLink';
+
+const ResourcesPage = () => {
+  return (
+    <div className="resources-page">
+      <h2>Resources & Assignments</h2>
+      <ResourceLink label="Assignments" botLink="https://t.me/UniHero_BOT?start=assignments" />
+      <ResourceLink label="Exam Prep" botLink="https://t.me/UniHero_BOT?start=examprep" />
+    </div>
+  );
+};
+
+export default ResourcesPage;
