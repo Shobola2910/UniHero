f1bf5c9148617e0270960995a8ed7d5865c58f4c
Shobola <shorustamovismoilbek2@gmail.com>
2025-11-13 18:22:51 -0500

    Update ResourceLink.tsx

--- components/ResourceLink.tsx
+++ components/ResourceLink.tsx
@@ -1,1 +1,26 @@
+// components/ResourceLink.tsx
 
+import Link from 'next/link';
+
+const ResourceLink = ({ label, botLink }) => {
+  return (
+    <div className="resource-link">
+      <h4>{label}</h4>
+      <Link href={botLink}>
+        <button>Access {label}</button>
+      </Link>
+    </div>
+  );
+};
+
+const ResourcesSection = () => {
+  return (
+    <div className="resources-section">
+      <h2>Resources</h2>
+      <ResourceLink label="Assignments" botLink="https://t.me/UniHero_BOT?start=assignments" />
+      <ResourceLink label="Exam Prep" botLink="https://t.me/UniHero_BOT?start=examprep" />
+    </div>
+  );
+};
+
+export default ResourcesSection;
