f6ab70615144a6ba60a17b77196608e1ab0048b2
Shobola <shorustamovismoilbek2@gmail.com>
2025-11-13 18:28:30 -0500

    Update podcasts.tsx

--- pages/podcasts.tsx
+++ pages/podcasts.tsx
@@ -1,1 +1,15 @@
+// pages/podcasts.tsx
+
+import StudyPodcasts from '../components/StudyPodcasts';
+
+const PodcastsPage = () => {
+  return (
+    <div className="podcasts-page">
+      <h2>Study Podcasts</h2>
+      <StudyPodcasts />
+    </div>
+  );
+};
+
+export default PodcastsPage;
 
