b94f4179c2537fc58e9ed0b5bff322e021728259
Shobola <shorustamovismoilbek2@gmail.com>
2025-11-13 18:25:39 -0500

    Create OrderResources.tsx

---
+++ components/OrderResources.tsx
@@ -0,0 +1,19 @@
+// components/OrderResources.tsx
+
+const OrderResources = () => {
+  return (
+    <div className="order-resources">
+      <h2>Order Resources & Assignments</h2>
+      <div className="button-group">
+        <a href="https://t.me/UniHero_BOT?start=assignments">
+          <button>Order Assignments</button>
+        </a>
+        <a href="https://t.me/UniHero_BOT?start=examprep">
+          <button>Get Exam Prep</button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default OrderResources;
