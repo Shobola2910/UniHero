d70bca446e01420f1ce154a3368b566278caf6d9
Shobola <shorustamovismoilbek2@gmail.com>
2025-11-13 18:23:34 -0500

    Create UniHeroHub.tsx

---
+++ components/UniHeroHub.tsx
@@ -0,0 +1,17 @@
+// components/UniHeroHub.tsx
+
+const UniHeroHub = () => {
+  return (
+    <div className="unihero-hub">
+      <h2>UniHero Hub</h2>
+      <p>Connect with us through these channels:</p>
+      <ul>
+        <li><a href="https://t.me/UniHero_news">📣 UniHero_News</a></li>
+        <li><a href="https://t.me/UniHero_BOT">🤖 UniHero Bot</a></li>
+        <li><a href="https://t.me/Unihero_admin">👨🏻‍💻 Admin</a></li>
+      </ul>
+    </div>
+  );
+};
+
+export default UniHeroHub;
